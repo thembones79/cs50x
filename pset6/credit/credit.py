25667ebe8294e1f6928dd35cdcd8a6131d50768b
thembones79 <michal.szumnarski@gmail.com>
2020-05-19 23:08:10 +0200

    completed credit.py

--- pset6/credit/credit.py
+++ pset6/credit/credit.py
@@ -1,149 +1,102 @@
-# include <stdio.h>
-# include <cs50.h>
+def main():
+    cardNumber = getPositiveLong("Number: ")
+    print(validateCardNumber(cardNumber))
 
-long getPositiveLong(string promptValue)
-string validateCardNumber(long cardNumber)
 
-int main(void)
-{
-    long cardNumber = getPositiveLong("Number: ")
-    printf("%s", validateCardNumber(cardNumber))
-}
-
-long getPositiveLong(string promptText)
-{
-    long positive
-    do
-    {
-        positive = get_long("%s", promptText)
-    } while (positive < 0)
+def getPositiveLong(promptText):
+    positive = -1
+    while (positive < 0):
+        positive = int(input(promptText))
     return positive
-}
 
-int sumOfEvenDigitsMultipliedBy2(long cardNumber)
-{
-    int counter = 0
-    int reminder = 0
-    int sumOfDoubleEvens = 0
-    int doubleReminder = 0
 
-    while (cardNumber > 0)
-    {
+def sumOfEvenDigitsMultipliedBy2(cardNumber):
+    counter = 0
+    reminder = 0
+    sumOfDoubleEvens = 0
+    doubleReminder = 0
+    while (cardNumber > 0):
         reminder = cardNumber % 10
-        cardNumber = cardNumber / 10
-        counter++
-        if (counter % 2 == 0)
-        {
+        cardNumber = cardNumber // 10
+        counter += 1
+        if (counter % 2 == 0):
             doubleReminder = reminder * 2
-            if (doubleReminder > 9)
-            {
+            if (doubleReminder > 9):
                 doubleReminder = doubleReminder - 9
-            }
             sumOfDoubleEvens = sumOfDoubleEvens + doubleReminder
-        }
-    }
-
     return sumOfDoubleEvens
-}
 
-int sumOfOddDigits(long cardNumber)
-{
-    int counter = 0
-    int reminder = 0
-    int sumOfOdds = 0
 
-    while (cardNumber > 0)
-    {
+def sumOfOddDigits(cardNumber):
+    counter = 0
+    reminder = 0
+    sumOfOdds = 0
+    while (cardNumber > 0):
         reminder = cardNumber % 10
-        cardNumber = cardNumber / 10
-        counter++
-        if (counter % 2 != 0)
-        {
+        cardNumber = cardNumber // 10
+        counter += 1
+        if (counter % 2 != 0):
             sumOfOdds += reminder
-        }
-    }
-
     return sumOfOdds
-}
 
-int checksum(long cardNumber)
-{
-    int sum = sumOfEvenDigitsMultipliedBy2(cardNumber) + sumOfOddDigits(cardNumber)
+
+def checksum(cardNumber):
+    sum = sumOfEvenDigitsMultipliedBy2(cardNumber) + sumOfOddDigits(cardNumber)
     return sum % 10
-}
 
-int countDigits(long cardNumber)
-{
-    int counter = 0
 
-    while (cardNumber > 0)
-    {
-        cardNumber = cardNumber / 10
-        counter++
-    }
-
+def countDigits(cardNumber):
+    counter = 0
+    while (cardNumber > 0):
+        cardNumber = cardNumber // 10
+        counter += 1
     return counter
-}
 
-int firstDigit(long cardNumber)
-{
-    int reminder = 0
 
-    while (cardNumber > 0)
-    {
+def firstDigit(cardNumber):
+    reminder = 0
+    while (cardNumber > 0):
         reminder = cardNumber % 10
-        cardNumber = cardNumber / 10
-    }
-
+        cardNumber = cardNumber // 10
     return reminder
-}
 
-long firstTwoDigits(long cardNumber)
-{
-    while (cardNumber > 99)
-    {
-        cardNumber = cardNumber / 10
-    }
 
+def firstTwoDigits(cardNumber):
+    while (cardNumber > 99):
+        cardNumber = cardNumber // 10
     return cardNumber
-}
 
-bool isVisa(long cardNumber)
-{
-    return ((countDigits(cardNumber) == 13 | | countDigits(cardNumber) == 16) & & firstDigit(cardNumber) == 4)
-}
 
-bool isAmEx(long cardNumber)
-{
-    return (countDigits(cardNumber) == 15 & & (firstTwoDigits(cardNumber) == 34 | | firstTwoDigits(cardNumber) == 37))
-}
+def isVisa(cardNumber):
+    return ((countDigits(cardNumber) == 13 or countDigits(cardNumber) == 16) and firstDigit(cardNumber) == 4)
 
-bool isMasterCard(long cardNumber)
-{
-    return (countDigits(cardNumber) == 16 & & (firstTwoDigits(cardNumber) >= 51 & & firstTwoDigits(cardNumber) <= 55))
-}
 
-string validateCardNumber(long cardNumber)
-{
-    if (checksum(cardNumber))
-    {
+def isAmEx(cardNumber):
+    return (countDigits(cardNumber) == 15 and (firstTwoDigits(cardNumber) == 34 or firstTwoDigits(cardNumber) == 37))
+
+
+def isMasterCard(cardNumber):
+    return (countDigits(cardNumber) == 16 and (firstTwoDigits(cardNumber) >= 51 and firstTwoDigits(cardNumber) <= 55))
+
+
+def validateCardNumber(cardNumber):
+    if (checksum(cardNumber)):
+
         return "INVALID\n"
-    }
 
-    if (isVisa(cardNumber))
-    {
+    if (isVisa(cardNumber)):
+
         return "VISA\n"
-    }
 
-    if (isAmEx(cardNumber))
-    {
+    if (isAmEx(cardNumber)):
+
         return "AMEX\n"
-    }
 
-    if (isMasterCard(cardNumber))
-    {
+    if (isMasterCard(cardNumber)):
+
         return "MASTERCARD\n"
-    }
 
     return "INVALID\n"
-}
+
+
+main()
